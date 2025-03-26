d34824065267ad4996801cd11675a958d78b555f
joao <joaomiguelcg54@gmail.com>
2025-03-26 16:45:03 -0300

    login

--- app.js
+++ app.js
@@ -3,26 +3,78 @@
 //npm install nodemon
 
 
-//importa o modulo do projeto
-const express =  require("express")
-const path = require("path")
 
-//configura o express para ser usado
-const App = express()
-App.set("view engine", "ejs")
-// Servindo arquivos estáticos (CSS, imagens, etc.) na pasta 'mvc e depois views'
-App.set("views",path.join(__dirname, "mvc/views" ))
-App.use(express.static(path.join(__dirname, "public")))
+// Importa os módulos
+const express = require("express");
+const path = require("path");
+const mysql = require("mysql2");
+
+// Configura o express
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "mvc/views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Conecta ao banco de dados
+const connection = mysql.createPool({
+  host: "127.0.0.1", 
+  user: "root", 
+  database: "meu_login", 
+  password: "" 
+});
 
 
-//Endpoint
-App.get("/", (req, res) => {
-    res.render("index.ejs", {
-        nome:"Carrion",
-        texto:"esta é uma demonstração"
-    }) 
-})
+
+// Conecta ao banco
+connection.on("connection", () => {
+  console.log("Conectado ao banco de dados!");
+});
 
 
-//deixar diponivel o serviço
-App.listen(3000, () => console.log("servidor ta rodandooooooooooooooooooo"))
+
+// Rota principal
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+
+
+// Rota de login
+app.post("/login", (req, res) => {
+  const usuario = req.body.usuario;
+  const senha = req.body.senha; 
+
+
+
+  // Verifica se os campos estão vazios ou se a consulta não encontrou o usuário
+  if (usuario === "" || senha === "") {
+    res.render("error.ejs", { 
+        mensagem: "Usuário ou senha inválidos!"
+    });
+  }
+
+
+
+  // Consulta o banco de dados
+  connection.query(
+    "SELECT * FROM login WHERE usuario = ? AND senha = ?",
+    [usuario, senha],
+    (err, results) => {
+
+      // Se não encontrar o usuário ou senha no banco
+      if (results.length === 0) {
+        res.render("error.ejs", { 
+           mensagem: "Usuário ou senha inválidos!" 
+        });
+      }
+
+      // Se o login for bem-sucedido, renderiza a página 'conta.ejs'  
+       res.render("conta.ejs");
+    }
+  );
+});
+
+
+app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
